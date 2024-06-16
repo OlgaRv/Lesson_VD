7ce9395dfdc6f3dd1f5aa552dfd45d77ed2d5fdc
OlgaRv <olgavr2959@gmail.com>
2024-06-16 18:59:56 +0300

    Examles Lesson VD05

--- L_05/main.py
+++ L_05/main.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def films():
-    return render_template('index.html')
-
+    context = {
+        "link": "Перейти в кинотеатр"
+    }
+    return render_template('index.html', **context)
 @app.route('/person/')
-def person():
-    return render_template('main.html')
+def films2():
+    context = {
+        "link": "Сходить в кинотеатр"
+    }
+    return render_template('main.html', **context)
 
 if __name__ == '__main__':
     app.run()
